96f231e7f578f8596d7aa208049083c30c8253f8
CrisConSer <cristinacondeserrano@gmail.com>
2023-10-28 09:17:29 +0200

    update

--- DesarrolloWebEntornoServidor/PHPBasico/Examen01/02-Ejercicio/index.php
+++ DesarrolloWebEntornoServidor/PHPBasico/Examen01/02-Ejercicio/index.php
@@ -10,24 +10,23 @@
 <?php
 
 //Crar un array asociativo con el horario de clase y mostrarlo por pantalla
-$horario=[];
 
-  $horario[""] = "";
+  $horario =
   [
-    'HORA'=>"15:15-16:15","16:15-17:15","17:15-18:15","18:15-18:30","18:30-19:30","19:30-20:30","20:30-21:30",
-    'lunes' => "DWEC","DWEC","DWEC","RECREO","DWES","DWES","DWES",
-    'martes' => "DWEC","DWEC","DWEC","RECREO","DAW","DAW","DAW",
-    'miercoles' =>"DWES","DWES","DWES","RECREO","DIW","DIW","EIE",
-    'jueves' => "DWES","DWES","DWES","RECREO","EIE","EIE","EIE",
-    'viernes' => "DIW","DIW","DIW","RECREO","HLC","HLC","HLC",
-    ];
+    'HORA'=>["HORA","15:15-16:15","16:15-17:15","17:15-18:15","18:15-18:30","18:30-19:30","19:30-20:30","20:30-21:30"],
+    'lunes' => ["LUNES","DWEC","DWEC","DWEC","RECREO","DWES","DWES","DWES"],
+    'martes' => ["MARTES","DWEC","DWEC","DWEC","RECREO","DAW","DAW","DAW",],
+    'miercoles' =>["MIERCOLES","DWES","DWES","DWES","RECREO","DIW","DIW","EIE"],
+    'jueves' => ["JUEVES","DWES","DWES","DWES","RECREO","EIE","EIE","EIE",],
+    'viernes' => ["VIERNES","DIW","DIW","DIW","RECREO","HLC","HLC","HLC",]
+  ];
 
 // Estilo CSS para la tabla
 echo '<style>';
 echo 'table {border-collapse: collapse; width: 50%; margin: 0 auto}';
-echo 'th{border: 1px solid black; padding: 8px; text-align: center;}';
-echo 'tr:nth-child(even) {background-color: #f2f2f2;}'; // Cambiar el color de fondo de filas pares
-echo 'td {border: 1px solid black;}'; // Definir el borde de las celdas
+echo 'th{border: 1px solid black; padding: 8px; text-align: center; font-family:Arial, Sans-serif}';
+echo 'tr:nth-child(even) {background-color: #f2f2f2; font-family:Arial, Sans-serif}'; // Cambiar el color de fondo de filas pares
+echo 'td {border: 1px solid black;font-family:Arial, Sans-serif}'; // Definir el borde de las celdas
 echo 'th {background-color: #178AE5; color: white;}'; // Cambiar el color de fondo de las cabeceras
 echo '</style>';  
 
@@ -35,16 +34,21 @@ echo '</style>';
 echo '<table border="5">';
 echo '<tr><th> HORARIO DE CLASE 2º DAW </th></tr>';
 
-//Con el foreach mostrar los datos del array
-foreach ($horario as $horas) {
-  echo '<tr>';
-  echo '<th>' . $horas['lunes'] . '</th>';
-  echo '<th>' . $horas['martes'] . '</th>';
-  echo '<th>' . $horas['miercoles'] . '</th>';
-  echo '<th>' . $horas['jueves'] . '</th>';
-  echo '<th>' . $horas['viernes'] . '</th>';
-  echo '</tr>';
-}
+echo '<table border="5">';
+    
+   // Con el for mostrar los datos del array
+   for ($i = 0; $i < count($horario['HORA']); $i++) {
+    echo '<tr>';
+    echo '<td>' . $horario['HORA'][$i] . '</td>';
+    echo '<td>' . $horario['lunes'][$i] . '</td>';
+    echo '<td>' . $horario['martes'][$i] . '</td>';
+    echo '<td>' . $horario['miercoles'][$i] . '</td>';
+    echo '<td>' . $horario['jueves'][$i] . '</td>';
+    echo '<td>' . $horario['viernes'][$i] . '</td>';
+    echo '</tr>';
+  }
+
+  echo '</table>';
 
 ?>
   
